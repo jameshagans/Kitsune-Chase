387e56ef17b03d574e49c3f718d1b6a7613d0252
Kevin Cho <kevcho604@gmail.com>
2023-05-15 14:01:28 -0700

    Add assets/tiles to project

--- public/js/helpers/bottomPlatform.js
+++ public/js/helpers/bottomPlatform.js
@@ -1,16 +1,16 @@
-export default function create() {
-  // Create a sprite for the bottom platform 
-  bottomPlatform = this.physics.add.sprite(game.config.width / 2, game.config.height, 'platform')
+// export default function create() {
+//   // Create a sprite for the bottom platform 
+//   bottomPlatform = this.physics.add.sprite(game.config.width / 2, game.config.height, 'platform')
 
-  // Set origin to center bottom
-  bottomPlatform.setOrigin(0.5, 1); 
+//   // Set origin to center bottom
+//   bottomPlatform.setOrigin(0.5, 1); 
 
-  // Enable physics for the platform
-  this.physics.world.enable(bottomPlatform); 
+//   // Enable physics for the platform
+//   this.physics.world.enable(bottomPlatform); 
 
-  // Make the platform immovable
-  bottomPlatform.body.setImmovable(true); 
+//   // Make the platform immovable
+//   bottomPlatform.body.setImmovable(true); 
 
-  // Add collision between plater and platform
-  this.physics.add.collider(player, bottomPlatform)
-}
+//   // Add collision between plater and platform
+//   this.physics.add.collider(player, bottomPlatform)
+// }
