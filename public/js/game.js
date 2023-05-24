0f6fdd6e90ec863c24284fc88bae4babb6e8e6c9
Kevin Cho <kevcho604@gmail.com>
2023-05-23 19:03:16 -0700

    Add v3 spritesheet

--- public/js/game.js
+++ public/js/game.js
@@ -36,7 +36,8 @@ const playerBPosition = [];
 
 function preload() {
   // this.load.image('player', 'assets/sprites/player_placeholder.png');
-  this.load.spritesheet('fox', '../assets/sprites/fox-V2.png', { frameWidth: 24, frameHeight: 18 });
+  // this.load.spritesheet('fox', '../assets/sprites/fox-V2.png', { frameWidth: 24, frameHeight: 18 });
+  this.load.spritesheet('fox', '../assets/sprites/fox-V3.png', { frameWidth: 26, frameHeight: 19 });
   this.load.image('stage_one_platform_ground', 'assets/tiles/foxgate-city-day-platform.PNG');
   this.load.image('stage_one_platform_roof-1-pink', 'assets/tiles/foxgate-city-day-platform-roof-1-pink.PNG');
   this.load.image('stage_one_platform_roof-2-orange', 'assets/tiles/foxgate-city-day-platform-roof-2-orange.PNG');
